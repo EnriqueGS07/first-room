96f9ebe00460ecc2e5c664f349f6b7628cc5a322
unknown <egonzalezsuarez07@gmail.com>
2023-05-18 18:19:57 -0500

    conf room

--- app/src/main/java/org/adaschool/retrofit/MainActivity.java
+++ app/src/main/java/org/adaschool/retrofit/MainActivity.java
@@ -1,18 +1,12 @@
 package org.adaschool.retrofit;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
+
 import android.os.Bundle;
-import android.util.Log;
-import com.bumptech.glide.Glide;
+
 import org.adaschool.retrofit.databinding.ActivityMainBinding;
-import org.adaschool.retrofit.RetrofitInstance;
-import org.adaschool.retrofit.BreedsListDto;
-import org.adaschool.retrofit.DogApiService;
 
 import java.util.List;
-import java.util.Map;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,63 +20,12 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        DogApiService dogApiService = RetrofitInstance.getRetrofitInstance().create(DogApiService.class);
+        AppDataBase db = Room.databaseBuilder(getApplicationContext(),
+                AppDataBase.class, "database-name").allowMainThreadQueries().build();
+        UserDao userDao = db.userDao();
+        userDao.inserOne(new User());
+        System.out.println(userDao.getAll());
 
-//        Call<BreedsListDto> call = dogApiService.getAllBreeds();
-//        call.enqueue(new Callback<BreedsListDto>() {
-//            @Override
-//            public void onResponse(Call<BreedsListDto> call, Response<BreedsListDto> response) {
-//                if (response.isSuccessful()) {
-//                    Map<String, String[]> breeds = response.body().getMessage();
-//                    for (Map.Entry<String, String[]> entry : breeds.entrySet()) {
-//                        Log.d(TAG, "Raza: " + entry.getKey());
-//                        for (String subRaza : entry.getValue()) {
-//                            Log.d(TAG, "Subraza: " + subRaza);
-//                        }
-//                    }
-//                } else {
-//                    Log.e(TAG, "Error en la respuesta de la API");
-//                }
-//            }
-//
-//            @Override
-//            public void onFailure(Call<BreedsListDto> call, Throwable t) {
-//                Log.e(TAG, "Error al llamar a la API", t);
-//            }
-//        });
-        loadDogInfo();
     }
-
-    private void loadDogInfo() {
-        MainActivity view = this;
-        String breed = "african";
-        DogApiService dogApiService = RetrofitInstance.getRetrofitInstance().create(DogApiService.class);
-        Call<BreedsListDto> call = dogApiService.getAllImages(breed);
-        call.enqueue(new Callback<BreedsListDto>() {
-
-            @Override
-            public void onResponse(Call<BreedsListDto> call, Response<BreedsListDto> response) {
-
-                if(response.isSuccessful()){
-                    assert response.body() != null;
-                    List<String> breeds = response.body().getMessage();
-                    String dogImageUrl = breeds.get(0);
-                    String dogName = breed;
-                    binding.textView.setText(dogName);
-                    Glide.with(view)
-                        .load(dogImageUrl)
-                        .into(binding.imageView);
-
-                }
-            }
-
-            @Override
-            public void onFailure(Call<BreedsListDto> call, Throwable t) {
-                Log.e(TAG, "Error al llamar a la API", t);
-            }
-        });
-//
-    }
-
 
 }
